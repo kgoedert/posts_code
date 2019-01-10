f6fca6d5f9c13bc2f42ccb2287534ce06c1c7a46
Kelly Goedert <kelly.goedert@gmail.com>
2019-01-10 12:42:37 -0200

    docker container

--- sails_docker/api/models/Movie.js
+++ sails_docker/api/models/Movie.js
@@ -1,12 +1,14 @@
 /**
- * User.js
+ * Movie.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
-  attributes: {},
-  autoCreatedAt: false,
-  autoUpdatedAt: false
+
+  attributes: {
+
+  }
 };
+
